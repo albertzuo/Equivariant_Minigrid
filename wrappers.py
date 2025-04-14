5ff462f8dd3847cf1332ef18109bbf86a29ad20e
Albert Zuo <albert.zuo@gmail.com>
2025-04-14 16:40:16 +0100

    change actions to only valid ones

--- wrappers.py
+++ wrappers.py
@@ -1,64 +1,67 @@
 import numpy as np
 import gymnasium as gym
+from gymnasium import ActionWrapper
+from gymnasium.spaces import Discrete
 from minigrid.wrappers import FullyObsWrapper
 
-class BaseWrapper(gym.ObservationWrapper):
-    def __init__(self, env, full_obs=True):
+class LimitedActionWrapper(ActionWrapper):
+    def __init__(self, env, allowed_actions):
+        super().__init__(env)
+        self.allowed_actions = allowed_actions
+        self.action_space = Discrete(len(allowed_actions))
+
+    def action(self, act):
+        return self.allowed_actions[act]
+
+class _MiniGridWrapperBase(gym.ObservationWrapper):
+    def __init__(self, env, full_obs=True, allowed_actions=[0, 1, 2]):
         if full_obs:
             env = FullyObsWrapper(env)
+        if allowed_actions:
+            env = LimitedActionWrapper(env, allowed_actions=allowed_actions)
         super().__init__(env)
         self.observation_space = env.observation_space.spaces['image']
+
+class BaseWrapper(_MiniGridWrapperBase):
+    def __init__(self, env, full_obs=True):
+        super().__init__(env, full_obs=full_obs)
     def observation(self, obs):
         return obs['image']
 
-class RandomRotateWrapper(gym.ObservationWrapper):
+class RandomRotateWrapper(_MiniGridWrapperBase):
     def __init__(self, env, subset=[0,1,2,3], full_obs=True):
-        if full_obs:
-            env = FullyObsWrapper(env)
-        super().__init__(env)
-        self.observation_space = env.observation_space.spaces['image']
+        super().__init__(env, full_obs=full_obs)
         self.subset = subset
         self.k = 0
-
     def reset(self, **kwargs):
         obs, info = self.env.reset(**kwargs)
         self.k = np.random.choice(self.subset)
         return self.observation(obs), info
-
     def observation(self, obs):
-        obs = obs['image']
-        img_rot = np.rot90(obs, k=self.k, axes=(0,1)).copy()
+        obs_img = obs['image'] 
+        img_rot = np.rot90(obs_img, k=self.k, axes=(0,1)).copy()
         return img_rot
 
-class Rotate90Wrapper(gym.ObservationWrapper):
+class Rotate90Wrapper(_MiniGridWrapperBase):
     def __init__(self, env, full_obs=True):
-        if full_obs:
-            env = FullyObsWrapper(env)
-        super().__init__(env)
-        self.observation_space = env.observation_space.spaces['image']
+        super().__init__(env, full_obs=full_obs)
     def observation(self, obs):
-        obs = obs['image']
-        img_rot = np.rot90(obs, k=1, axes=(0,1)).copy()
+        obs_img = obs['image'] 
+        img_rot = np.rot90(obs_img, k=1, axes=(0,1)).copy()
         return img_rot
 
-class Rotate180Wrapper(gym.ObservationWrapper):
+class Rotate180Wrapper(_MiniGridWrapperBase):
     def __init__(self, env, full_obs=True):
-        if full_obs:
-            env = FullyObsWrapper(env)
-        super().__init__(env)
-        self.observation_space = env.observation_space.spaces['image']
+        super().__init__(env, full_obs=full_obs)
     def observation(self, obs):
-        obs = obs['image']
-        img_rot = np.rot90(obs, k=2, axes=(0,1)).copy()
+        obs_img = obs['image'] 
+        img_rot = np.rot90(obs_img, k=2, axes=(0,1)).copy()
         return img_rot
 
-class Rotate270Wrapper(gym.ObservationWrapper):
+class Rotate270Wrapper(_MiniGridWrapperBase):
     def __init__(self, env, full_obs=True):
-        if full_obs:
-            env = FullyObsWrapper(env)
-        super().__init__(env)
-        self.observation_space = env.observation_space.spaces['image']
+        super().__init__(env, full_obs=full_obs)
     def observation(self, obs):
-        obs = obs['image']
-        img_rot = np.rot90(obs, k=3, axes=(0,1)).copy()
+        obs_img = obs['image'] 
+        img_rot = np.rot90(obs_img, k=3, axes=(0,1)).copy()
         return img_rot
