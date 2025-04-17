34ea36ce7b9116ab9b51170113a05d5878f8df3d
Albert Zuo <albert.zuo@gmail.com>
2025-04-17 17:43:10 +0100

    add mostly and fully equivariant checkpoints+code

--- models/features_extractor.py
+++ models/features_extractor.py
@@ -29,6 +29,64 @@ class MinigridFeaturesExtractor(BaseFeaturesExtractor):
     def forward(self, observations: torch.Tensor) -> torch.Tensor:
         return self.linear(self.cnn(observations))
 
+class MostlyC4EquivariantCNN(BaseFeaturesExtractor):
+    def __init__(self, observation_space, features_dim=256):
+        super().__init__(observation_space, features_dim)
+        
+        self.r2_act = gspaces.rot2dOnR2(N=4)
+        
+        n_input_channels = observation_space.shape[0]        
+        self.input_type = enn.FieldType(self.r2_act, n_input_channels * [self.r2_act.trivial_repr])
+        
+        self.equivariant_model = enn.SequentialModule(
+            enn.SequentialModule(
+                enn.R2Conv(self.input_type, 
+                           enn.FieldType(self.r2_act, 32 * [self.r2_act.regular_repr]),
+                           kernel_size=3, 
+                           padding=1, 
+                           stride=1),
+                enn.ReLU(enn.FieldType(self.r2_act, 32 * [self.r2_act.regular_repr])),
+                enn.PointwiseMaxPool(enn.FieldType(self.r2_act, 32 * [self.r2_act.regular_repr]), 2)
+            ),
+            
+            enn.SequentialModule(
+                enn.R2Conv(enn.FieldType(self.r2_act, 32 * [self.r2_act.regular_repr]),
+                           enn.FieldType(self.r2_act, 64 * [self.r2_act.regular_repr]),
+                           kernel_size=3, 
+                           padding=1, 
+                           stride=1),
+                enn.ReLU(enn.FieldType(self.r2_act, 64 * [self.r2_act.regular_repr])),
+                enn.PointwiseMaxPool(enn.FieldType(self.r2_act, 64 * [self.r2_act.regular_repr]), 2)
+            ),
+            
+            enn.SequentialModule(
+                enn.R2Conv(enn.FieldType(self.r2_act, 64 * [self.r2_act.regular_repr]),
+                           enn.FieldType(self.r2_act, 64 * [self.r2_act.regular_repr]),
+                           kernel_size=3, 
+                           padding=1, 
+                           stride=1),
+                enn.ReLU(enn.FieldType(self.r2_act, 64 * [self.r2_act.regular_repr])),
+                enn.PointwiseMaxPool(enn.FieldType(self.r2_act, 64 * [self.r2_act.regular_repr]), 2)
+            )
+        )
+        
+        self.group_pool = enn.GroupPooling(self.equivariant_model[-1].out_type)
+        
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(self.group_pool.out_type.size, features_dim),
+            nn.ReLU()
+        )
+    
+    def forward(self, observations):
+        device = next(self.parameters()).device
+        x = enn.GeometricTensor(observations.to(device), self.input_type)
+        x = self.equivariant_model(x)
+        x = self.group_pool(x)
+        x = x.tensor
+        x = self.fc(x.reshape(x.shape[0], -1))
+        return x
+
 class SmallKernelC4EquivariantCNN(BaseFeaturesExtractor):
     def __init__(self, observation_space, features_dim=256):
         super().__init__(observation_space, features_dim)
